198c870fb5c040e9d13b5689389927d34c06e1cc
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2024-08-14 23:36:26 +0200

    update code following insta chages

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-import { readFile } from 'fs/promises';
+import { readFile } from "fs/promises";
 import { getIgResponse } from "./http.js";
-import 'dotenv/config';
-
-const { userId, sessionId, followersQueryHash, followingQueryHash } = process.env;
 
 const toUnfollow = [];
 const following = [];
@@ -10,66 +7,56 @@ const followers = [];
 const exclude = [];
 
 const getExclude = async (file) => {
-   // iterate over files in following folder
-   exclude.push(...JSON.parse(await readFile("./exclude.json", "utf-8")));  
-}
+  // iterate over files in following folder
+  exclude.push(...JSON.parse(await readFile("./exclude.json", "utf-8")));
+};
 
 const compare = () => {
-   following.forEach((followingAcc) => {
-      if (!followers.includes(followingAcc) && !exclude.includes(followingAcc)) {
-         toUnfollow.push(followingAcc);
-      }
-   });
+  following.forEach((followingAcc) => {
+    if (!followers.includes(followingAcc) && !exclude.includes(followingAcc)) {
+      toUnfollow.push(followingAcc);
+    }
+  });
 
-   console.warn(`You should unfollow ${toUnfollow.toString()}`);
-}
+  console.warn(`You should unfollow ${toUnfollow.toString()}`);
+};
 
 const main = async () => {
-   await getExclude();
+  await getExclude();
 
-   let hasNextPage = false;
-   let nextPage = "";
+  let hasNextPage = false;
+  let nextPage = "";
 
-   do {
-      const data = (await getIgResponse(
-         userId,
-         sessionId, 
-         nextPage,
-         "follow_list_page",
-         "followers"
-      ));
+  do {
+    const data = await getIgResponse(nextPage, "follow_list_page", "followers");
 
-      nextPage = data.next_max_id;
-      hasNextPage = !!nextPage;
-      
-      followers.push(...data.users
-      .map((user) => user.username)
-      .filter((username) => !!username));
-      
-   } while (hasNextPage);
+    nextPage = data.next_max_id;
+    hasNextPage = !!nextPage;
 
-   console.info(`You have ${followers.length} followers`);
-   
-   do {
-      const data = (await getIgResponse(
-         userId,
-         sessionId, 
-         nextPage,
-         "follow_list_page",
-         "following"
-      ));
-      
-      nextPage = data.next_max_id;
-      hasNextPage = !!nextPage;
-      
-      following.push(...data.users
-      .map((user) => user.username)
-      .filter((username) => !!username));
-      
-   } while (hasNextPage);
-   
-   console.info(`You are following ${following.length} people`);
-   compare();
-}
+    followers.push(
+      ...data.users
+        .map((user) => user.username)
+        .filter((username) => !!username),
+    );
+  } while (hasNextPage);
+
+  console.info(`You have ${followers.length} followers`);
+
+  do {
+    const data = await getIgResponse(nextPage, "follow_list_page", "following");
+
+    nextPage = data.next_max_id;
+    hasNextPage = !!nextPage;
+
+    following.push(
+      ...data.users
+        .map((user) => user.username)
+        .filter((username) => !!username),
+    );
+  } while (hasNextPage);
+
+  console.info(`You are following ${following.length} people`);
+  compare();
+};
 
 main();
--- http.js
+++ http.js
@@ -1,20 +1,35 @@
 import axios from "axios";
+import "dotenv/config";
 
-export const getIgResponse = async (userId, sessionId, nextPage, searchSurface, endpoint) => {
-    const BASE_URL = "https://www.instagram.com/api/v1/friendships/2944763552/";
-    const headers = {
-        'Cookie': `ds_user_id=${userId};sessionid=${sessionId};`,
-        'User-Agent':'Mozilla/5.0 (Linux; Android 8.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)'
-    };
-    const queryParams = new URLSearchParams();
-    queryParams.set("count", 200);
+const { userId, sessionId, crsftoken, XASBDID, XIGAppID, XIGWWWClaim } =
+  process.env;
 
-    if (nextPage) {
-        queryParams.set("max_id", nextPage);
-    }
-    // queryParams.set("search_surface", searchSurface)
+export const getIgResponse = async (nextPage, searchSurface, endpoint) => {
+  const BASE_URL = `https://www.instagram.com/api/v1/friendships/${userId}/`;
+  const headers = {
+    Cookie: `ds_user_id=${userId};sessionid=${sessionId};csrftoken=${crsftoken};`,
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:129.0) Gecko/20100101 Firefox/129.0",
+    Origin: "https://www.instagram.com",
+    Referer: "https://www.instagram.com/",
+    "X-CSRFToken": crsftoken,
+    "X-ASBD-ID": XASBDID,
+    "X-IG-App-ID": XIGAppID,
+    "X-IG-WWW-Claim": XIGWWWClaim,
+    "X-Requested-With": "XMLHttpRequest",
+  };
+  const queryParams = new URLSearchParams();
+  queryParams.set("count", 200);
 
-    return (await axios.get(`${BASE_URL}${endpoint}?${queryParams.toString()}`, {
-        headers,
-    })).data;
-}
+  if (nextPage) {
+    queryParams.set("max_id", nextPage);
+  }
+
+  queryParams.set("search_surface", searchSurface);
+
+  return (
+    await axios.get(`${BASE_URL}${endpoint}?${queryParams.toString()}`, {
+      headers,
+    })
+  ).data;
+};
